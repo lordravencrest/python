386e0b7094f5b82e3af4d60f4d0df71675740146
lordravencrest <starfallfaerie@gmail.com>
2021-08-09 11:05:25 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,38 @@
+duration = int(input('Введите время в секундах: '))
+days = duration // (60 * 60 * 24)
+hours = (duration - days * (60 * 60 * 24)) // (60 * 60)
+minutes = (duration - days * (60 * 60 * 24) - hours * (60 * 60)) // 60
+seconds = duration - days * (60 * 60 * 24) - hours * (60 * 60) - minutes * 60
+print(days, 'дн', hours, 'час', minutes, 'мин', seconds, 'сек')
+
+
+my_list = []
+for num in range(1, 1001, 2):
+    my_list.append(num ** 3)
+final_sum = 0
+for num in my_list:
+    check_sum = 0
+    for check_num in str(num):
+        check_sum += int(check_num)
+    if check_sum % 7 == 0:
+        final_sum += num
+print(final_sum)
+final_sum = 0
+for num in my_list:
+    num += 17
+    check_sum = 0
+    for check_num in str(num):
+        check_sum += int(check_num)
+    if check_sum % 7 == 0:
+        final_sum += num
+print(final_sum)
+
+
+percent = int(input('Введите число процента: '))
+if percent == 1:
+    word = 'процент'
+elif percent <= 4:
+    word = 'процента'
+else:
+    word = 'процентов'
+print(percent, word)
