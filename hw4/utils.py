a38a018f7a8f27636bc537ce2729a21a8029b25f
lordravencrest <starfallfaerie@gmail.com>
2021-08-19 18:15:37 +0300

    добавила практическое задание №1, 4

--- hw4/utils.py
+++ hw4/utils.py
@@ -1,4 +1,3 @@
-
 import requests
 from decimal import *
 from datetime import datetime
@@ -14,9 +13,8 @@ def currency_rates(val):
         return None
 
     rub = response[response.find('<Value>', response.find(val)) + 7:response.find('</Value>', response.find(val))]
-    day_raw = response[response.find('Date="') + 6:response.find("'", response.find('Date="') + 6)].split('.')
-    day, month, year = map(int, day_raw)
-    return f"{Decimal(rub.replace(',', '.'))}, {datetime(day=day, month=month, year=year)}"
+    print(rub)
+
 
 print(currency_rates('USD'))
 print(currency_rates('EUR'))
